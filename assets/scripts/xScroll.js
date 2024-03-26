83da8426d170ff9b99312e83755a0eb5868e0958
binghamxsolutions <binghamxsolutions@outlook.com>
2024-03-25 22:13:48 -0400

    Removed the Ko-fi link and adjusted the footer layout to re-fit its contents

--- assets/scripts/xScroll.js
+++ assets/scripts/xScroll.js
@@ -4,23 +4,26 @@ let mediaQueryTest = window.matchMedia("(max-width: 239px)");
 function toTop() {
 	btn = document.getElementsByClassName("to-top");
 	otherBtnExists = false;
-	
-	if (btn.length > 1) { //checks to see if another button exists
-		otherBtn = true;
-	}
-	
-	if ((document.body.scrollTop > 30) || (document.documentElement.scrollTop > 30)) {
-		btn[0].style.display = "block";
+
+	if (btn.length != 0) {
+		if (btn.length > 1) { //checks to see if another button exists
+			otherBtn = true;
+		}
 		
-		if (otherBtnExists == true) {// only shows the second to-top button if there are more than one
-			btn[1].style.display = "block";	
-		}
-	} else {
-		btn[0].style.display = "none";
-		if (otherBtnExists) {// only shows the second to-top button if there are more than one
-			btn[1].style.display = "block";	
-		}
-	}	
+		if ((document.body.scrollTop > 30) || (document.documentElement.scrollTop > 30)) {
+			btn[0].style.display = "block";
+			
+			if (otherBtnExists == true) {// only shows the second to-top button if there are more than one
+				btn[1].style.display = "block";	
+			}
+		} else {
+			btn[0].style.display = "none";
+			if (otherBtnExists) {// only shows the second to-top button if there are more than one
+				btn[1].style.display = "block";	
+			}
+		}	
+		
+	}
 }
 
 
